409f7e5b2c4a45ce54cccb51ad6796a4fede3e55
Evan_MacMini <evan036@gmail.com>
2019-01-03 21:07:19 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Evan on 2019/1/3.
+//  Copyright © 2019年 MyGoodies Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
